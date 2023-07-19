989028873c3b2f77343f2b9de11f2fbcff22aa7e
ismagilov-anton <ismagilov-anton@bauart.pro>
2023-07-19 17:45:45 +0700

    я знал что ты посмотришь

---
+++ src/components/ProfileGamer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './ProfileGamer.scss'
+
+export default class ProfileGamer extends Component {
+    render() {
+        return (
+            <div className='profile'>
+                <div className='profile__img'>
+                    <img src='https://avatars.mds.yandex.net/i?id=83ddc007e675bab73d435d6a0a7eacbef5e25746-9182174-images-thumbs&n=13' />
+                </div>
+                <div className='profile__info'>
+                    <div>
+                        <div>
+                            div
+                        </div>
+                        <img/>
+                    </div>
+                    <div>
+                    
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
